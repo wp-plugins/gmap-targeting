51f162e2b587656d5d98608f44a643afe8475c92
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 21:40:17 +0000

    obsolete plugin files
    git-svn-id: https://plugins.svn.wordpress.org/gmap-targeting/trunk@818464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/front_js.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-  var gmt_ajax_url="<?= GMT_PLUGIN_LINK ?>index.php";
-</script>
--- view/admin/admin_header.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-  var gmt_wp_site_url="<?= site_url() ?>";
-  var gmt_ajax_url="<?=GMT_PLUGIN_LINK . "index.php"?>";
-  var gmt_plugin_url="<?=GMT_PLUGIN_LINK?>";
-</script>
-<div class="wrap">
-  <?= (isset($message) ? $message . "<br />" : "") ?>
-
-
